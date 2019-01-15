c9603c8df7dc95caf83a31698480dff9721d116d
Garth Fritz <garth.fritz@gmail.com>
2019-01-15 07:43:22 -0500

    split Client.fs into View, Types, and State modules, and leave Client as the Elmish entry point

---
+++ beer2/src/Client/State.fs
@@ -0,0 +1,43 @@
+module Client.State
+
+open Elmish
+
+open Fable.PowerPack.Fetch
+
+open Thoth.Json
+
+open Shared
+
+open Types
+
+
+
+// The update function computes the next state of the application based on the current state and the incoming events/messages
+// It can also run side-effects (encoded as commands) like calling the server via Http.
+// these commands in turn, can dispatch messages to which the update function will react.
+let update (msg : Msg) (currentModel : Model) : Model * Cmd<Msg> =
+    match currentModel.Counter, msg with
+    | Some counter, Increment ->
+        let nextModel = { currentModel with Counter = Some { Value = counter.Value + 1 } }
+        nextModel, Cmd.none
+    | Some counter, Decrement ->
+        let nextModel = { currentModel with Counter = Some { Value = counter.Value - 1 } }
+        nextModel, Cmd.none
+    | _, InitialCountLoaded (Ok initialCount)->
+        let nextModel = { Counter = Some initialCount }
+        nextModel, Cmd.none
+
+    | _ -> currentModel, Cmd.none
+
+let initialCounter = fetchAs<Counter> "/api/init" (Decode.Auto.generateDecoder())
+
+// defines the initial state and initial command (= side-effect) of the application
+let init () : Model * Cmd<Msg> =
+    let initialModel = { Counter = None }
+    let loadCountCmd =
+        Cmd.ofPromise
+            initialCounter
+            []
+            (Ok >> InitialCountLoaded)
+            (Error >> InitialCountLoaded)
+    initialModel, loadCountCmd
---
+++ beer2/src/Client/View.fs
@@ -0,0 +1,56 @@
+module Client.View
+
+open Fable.Helpers.React
+open Fable.Helpers.React.Props
+
+open Fulma
+
+open Types
+
+
+let safeComponents =
+    let components =
+        span [ ]
+           [
+             a [ Href "https://saturnframework.github.io" ] [ str "Saturn" ]
+             str ", "
+             a [ Href "http://fable.io" ] [ str "Fable" ]
+             str ", "
+             a [ Href "https://elmish.github.io/elmish/" ] [ str "Elmish" ]
+             str ", "
+             a [ Href "https://mangelmaxime.github.io/Fulma" ] [ str "Fulma" ]
+           ]
+
+    p [ ]
+        [ strong [] [ str "SAFE Template" ]
+          str " powered by: "
+          components ]
+
+let button txt onClick =
+    Button.button
+        [ Button.IsFullWidth
+          Button.Color IsPrimary
+          Button.OnClick onClick ]
+        [ str txt ]
+
+let show = function
+| { Counter = Some counter } -> string counter.Value
+| { Counter = None   } -> "Loading..."
+
+let view (model : Model) (dispatch : Msg -> unit) =
+    div []
+        [ Navbar.navbar [ Navbar.Color IsPrimary ]
+            [ Navbar.Item.div [ ]
+                [ Heading.h2 [ ]
+                    [ span [] [ str "Libations Notifier" ] ] ] ]
+
+          Container.container []
+              [ Content.content [ Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
+                    [ Heading.h3 [] [ str ("Press buttons to manipulate counter: " + show model) ] ]
+                Columns.columns []
+                    [ Column.column [] [ button "-" (fun _ -> dispatch Decrement) ]
+                      Column.column [] [ button "+" (fun _ -> dispatch Increment) ] ] ]
+
+          Footer.footer [ ]
+                [ Content.content [ Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
+                    [ safeComponents ] ] ]
